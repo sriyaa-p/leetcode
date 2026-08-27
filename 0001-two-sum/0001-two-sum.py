ef89eac216df89a04e251052a8408c62b5de68fd
Sriya <sriya2005panda@gmail.com>
2026-08-27 23:10:59 +0530

    Time: 0 ms (100%), Space: 20.5 MB (41.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Hash Map is the Optimal approach for the given Solution when the array is not sorted
         hash_map={}
         for i,num in enumerate(nums):
             complement = target-num
             if complement in hash_map:
                 return [hash_map[complement],i]
             hash_map[num]=i
+
+        '''
+        #Using two pointer approach not optimal solution takes O(nlogn) time
+        indexed_nums=[(num,i) for i,num in enumerate(nums)]
+        indexed_nums.sort()
+        left=0
+        right=len(indexed_nums)-1
+        while left<right:
+            current_sum=indexed_nums[left][0]+indexed_nums[right][0]
+            if current_sum==target:
+                return indexed_nums[left][1],indexed_nums[right][1]
+            elif current_sum<target:
+                left+=1
+            else:
+                right-=1
+        return []
+        '''
