27a66d00862e00ef270572787da5537acc5ba5cc
Sriya <sriya2005panda@gmail.com>
2026-09-03 00:26:30 +0530

    Time: 10 ms (27.61%), Space: 24.9 MB (12.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,25 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        Method uses a O(nlogn) time complexity
         hashmap={}
         for i,num in enumerate(nums):
             hashmap[num]=hashmap.get(num,0)+1
         sorted_nums=sorted(hashmap,  key=hashmap.get, reverse=True)
         return sorted_nums[:k]
+        '''
+        hashmap={}
+        for num in nums:
+            hashmap[num]=hashmap.get(num,0)+1
+        # create bucket
+        bucket=[[] for _ in range(len(nums)+1)]
+        # add to bucket
+        for num,freq in hashmap.items():
+            bucket[freq].append(num)
+        # Highest Frequency first
+        result=[]
+        for freq in range(len(nums),0,-1):
+            for num in bucket[freq]:
+                result.append(num)
+                if len(result)==k:
+                    return result
