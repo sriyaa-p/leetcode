beb92e143990a717641f7c43d4a1112ecb3f118c
Sriya <sriya2005panda@gmail.com>
2026-09-07 00:36:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,5 +5,21 @@ class Solution:
         """
         #Arrays are sorted in Ascending order
         # m=number of elements in nums1 and n=number of elements in nums2
+        # This is the easiest method
+        '''
         nums1 [m: ] = nums2
         nums1.sort()
+        '''
+        # Using Two pointers
+        last = m+n-1
+        while m>0 and n>0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last]=nums1[m-1]
+                m-=1
+            else:
+                nums1[last]=nums2[n-1]
+                n-=1
+            last-=1
+        while n>0:
+            nums1[last]=nums2[n-1]
+            n,last=n-1,last-1
